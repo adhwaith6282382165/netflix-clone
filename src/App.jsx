0d91f09cbeecb36e872771ae2b792b7b88c9c9df
adhwaith6282382165 <adwaithcreji321@gmail.com>
2025-02-28 12:55:51 +0530

    netflix

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import LandingPage from "./pages/LandingPage"
+import WatchHistory from "./pages/WatchHistory"
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+
+      <Home />
+
+      <LandingPage />
+
+      <WatchHistory />
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
